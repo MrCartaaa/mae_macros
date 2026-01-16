94bb45f7c42bbb0048532c3d069b3b3f7cc7869b
Carter Steele <carter@maetech.ca>
2026-01-16 12:24:17 -0500

    merging all macro libs

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 #![deny(unsafe_op_in_unsafe_fn)]
 
 #[allow(non_camel_case_types, nonstandard_style)]
-mod util;
+extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
@@ -13,12 +13,51 @@ use syn::{
     Data::Struct,
     DataStruct, DeriveInput, Fields,
     Fields::Named,
-    FieldsNamed, Ident, LitStr, Token,
+    FieldsNamed, Ident, ItemFn, LitStr, Token,
     parse::{Parse, ParseStream},
     parse_macro_input,
 };
 
+mod util;
 use util::*;
+
+#[proc_macro_attribute]
+pub fn run_app(_: TokenStream, input: TokenStream,) -> TokenStream {
+    let input_fn = parse_macro_input!(input as ItemFn);
+
+    let fn_block = &input_fn.block.stmts[0];
+
+    quote! {
+    async fn run<Context: Clone + Send + 'static>(
+        listener: TcpListener,
+        db_pool: PgPool,
+        base_url: String,
+        hmac_secret: SecretString,
+        redis_uri: SecretString,
+        custom_context: Context,
+    ) -> Result<Server, anyhow::Error> {
+
+         let redis_store = app::redis_session(redis_uri).await?;
+         let server = HttpServer::new(move || {
+             ActixWebApp::new()
+                 .wrap(TracingLogger::default())
+                 .wrap(app::session_middleware(
+                     hmac_secret.clone(),
+                     redis_store.clone(),
+                 ))
+                 .app_data(web::Data::new(ApplicationBaseUrl(base_url.clone())))
+                 .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
+                 .app_data(web::Data::new(db_pool.clone()))
+                 .app_data(web::Data::new(custom_context.clone()))
+             .#fn_block
+         })
+         .listen(listener)?
+         .run();
+         Ok(server)
+         }
+         }
+    .into()
+}
 
 struct Args {
     ctx: Ident,
@@ -27,17 +66,13 @@ struct Args {
 }
 
 impl Parse for Args {
-    fn parse(input: ParseStream<'_>) -> syn::Result<Self> {
-        Ok(Self {
-            ctx: input.parse()?,
-            _comma: input.parse()?,
-            schema: input.parse()?,
-        })
+    fn parse(input: ParseStream<'_,>,) -> syn::Result<Self,> {
+        Ok(Self { ctx: input.parse()?, _comma: input.parse()?, schema: input.parse()?, },)
     }
 }
 
 #[proc_macro_attribute]
-pub fn schema(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn schema(args: TokenStream, input: TokenStream,) -> TokenStream {
     let Args { ctx, schema, .. } = parse_macro_input!(args as Args);
     let ast = parse_macro_input!(input as DeriveInput);
 
@@ -46,10 +81,7 @@ pub fn schema(args: TokenStream, input: TokenStream) -> TokenStream {
 
     // confirm the macro is being called on a Struct Type and extract the fields.
     let fields = match ast.data {
-        Struct(DataStruct {
-            fields: Named(FieldsNamed { ref named, .. }),
-            ..
-        }) => named,
+        Struct(DataStruct { fields: Named(FieldsNamed { ref named, .. },), .. },) => named,
         _ => unimplemented!("Only works for structs"),
     };
 
@@ -62,7 +94,7 @@ pub fn schema(args: TokenStream, input: TokenStream) -> TokenStream {
             #(#attrs)*
             pub #name: #ty
         }
-    });
+    },);
 
     // rebuild repo struct with the existing fields and default fields for the repo
     // NOTE: here, we are deriving the Repo with the proc_macro_derive fn from above
@@ -101,21 +133,18 @@ pub fn schema(args: TokenStream, input: TokenStream) -> TokenStream {
 //     BindArgs
 
 #[proc_macro_derive(MaeRepo, attributes(id, from_context, gen_date))]
-pub fn derive_mae_repo(item: TokenStream) -> TokenStream {
+pub fn derive_mae_repo(item: TokenStream,) -> TokenStream {
     let ast = parse_macro_input!(item as DeriveInput);
 
     // Making sure it the derive macro is called on a struct;
     let _ = match &ast.data {
-        Struct(DataStruct {
-            fields: Fields::Named(fields),
-            ..
-        }) => &fields.named,
+        Struct(DataStruct { fields: Fields::Named(fields,), .. },) => &fields.named,
         _ => panic!("expected a struct with named fields"),
     };
 
-    let (repo_option, _) = as_option(&ast);
-    let (repo_typed, _) = as_typed(&ast);
-    let (repo_variant, _) = as_variant(&ast);
+    let (repo_option, _,) = as_option(&ast,);
+    let (repo_typed, _,) = as_typed(&ast,);
+    let (repo_variant, _,) = as_variant(&ast,);
 
     quote! {
         #repo_option
